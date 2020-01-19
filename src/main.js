fc61b444aac391d9290ad7545a7dcf9eabf19ede
baitu.huang <baitu.huang@shopee.com>
2020-01-19 20:19:02 +0800

    80

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Reveal from 'reveal.js'
 import Prism from 'prismjs'
 
 if (process.env.NODE_ENV === 'development') {
-  document.querySelector('.slides').innerHTML = require('./aaa/index.pug')
+  document.querySelector('.slides').innerHTML = require('./vue-ssr/index.pug')
 }
 
 Reveal.initialize({
@@ -10,6 +10,21 @@ Reveal.initialize({
   fragmentInURL: true,
   history: true,
   transition: 'concave'
+})
+
+function litdom (selector, callback) {
+  const selectors = Array.isArray(selector) ? selector : [selector]
+  selectors.forEach(s => {
+    document.querySelectorAll(s).forEach((...args) => {
+      callback(...args, s)
+    })
+  })
+}
+
+litdom(['pre', 'code'], d => {
+  if (!d.className) {
+    d.className = 'language-javascript'
+  }
 })
 
 Prism.highlightAll()
